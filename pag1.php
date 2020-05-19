195381d9a64a464a26d1c380fce1498118dbba82
Bolaxano <alexti.cav@gmail.com>
2020-05-18 21:11:03 -0300

    Layout com Pag1

--- pag1.php
+++ pag1.php
@@ -1,11 +1,12 @@
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
 <?php 
-  include('layout/header.php');
+include('layout/header.php');
+?>
 
 
- ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-light fixed-top background-purple">
-  <a class="navbar-brand" href="home.php">Adotaqui! <i class="fas fa-paw"></i></a>
+  <a class="navbar-brand margem" href="home.php">Adotaqui! <i class="fas fa-paw"></i></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -41,8 +42,80 @@
   </div>
 </nav>
 
+<img class="img1" src="img/img1.jpg">
 
+<div class="card card1 pintado">
+  <div class="card-body">
+    <form>
+      <h2 style="text-align: center;" class="fonte">Encontre um novo amigo</h2>
+      <br>
+      <div class="row">
+        <div class="form-group col-6">
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option selected disabled>Estado</option>
+            <option>DF</option>
+            <option>GO</option>
+            <option>SP</option>
+            <option>RJ</option>
+            <option>...</option>
+          </select>
+        </div>
+        <div class="form-group col-6">
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option selected disabled>Cidade</option>
+            <option></option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="form-group col-6">
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option selected disabled>Porte</option>
+            <option>Pequeno</option>
+            <option>Médio</option>
+            <option>Grande</option>
+          </select>
+        </div>
+        <div class="form-group col-6">
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option selected disabled>Cidade</option>
+            <option></option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="form-group col-6">
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option selected disabled>Porte</option>
+            <option>Pequeno</option>
+            <option>Médio</option>
+            <option>Grande</option>
+          </select>
+        </div>
+        <div class="form-group col-6">
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option selected disabled>Cidade</option>
+            <option></option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+      </div>
+      <br>
+      <button type="button" class="btn btn-success centro">Success</button>
+    </form>
+  </div>
+</div>
 
 <?php 
-  include('layout/footer.php');
- ?>
+include('layout/footer.php');
+?>
