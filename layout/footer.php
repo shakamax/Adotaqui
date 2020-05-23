9a051ea82bceba6d6455d31ef0902fd74f459482
João Guilherme Machado <guilherme.estudioweb@gmail.com>
2020-05-23 15:53:15 -0300

    Atualizar perfil incompleto, animação com jquery

--- layout/footer.php
+++ layout/footer.php
@@ -6,7 +6,8 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery-3.5.1.slim.min.js" type="text/javascript"></script>
+
+    <script src="js/jquery.3.4.1.js" type="text/javascript"></script>
     <script src="js/popper.min.js" type="text/javascript"></script>
     <script src="js/bootstrap.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="fontawesome/js/all.min.js"></script>
@@ -24,11 +25,54 @@
        $('.conteudo').css({ paddingBottom: hf });
 
        $('.js-example-basic-multiple').select2();
+       $('.js-example-basic-single').select2();
 
        $(".js-example-basic-multiple").select2({
             tags: true,
             tokenSeparators: [',', ' ']
-        })
+        });
+
+        $('#cep').change(function() {
+            var cep = $(this).val();
+            $.ajax({
+                url: "https://viacep.com.br/ws/" + cep + "/json/",
+                type: 'GET',
+                dataType: 'json',
+                beforeSend: function () {
+                    $('#logradouro').val("...");
+                    $('#bairro').val("...");
+                    $('#localidade').val("...");
+                    $('#uf').val("...");
+                },
+                success: function (data) {
+                    $('#logradouro').val(data.logradouro);
+                    $('#bairro').val(data.bairro);
+                    $('#localidade').val(data.localidade);
+                    $("#uf option").filter(function() {
+                      return this.val() == data.uf; 
+                    }).attr('selected', true);
+                }
+            });
+        });
+
+        $('#btnConta').click(function() {
+          $('.dados').hide('slow');
+          $('#btnDados').removeClass('selecionado');
+          $('#btnDados').addClass('btn-list');
+          $('#btnConta').removeClass('btn-list');
+          $('#btnConta').addClass('selecionado');
+          $('.login').show('slow');
+        });
+
+        $('#btnDados').click(function() {
+          $('.login').hide('slow');
+          $('#btnConta').removeClass('selecionado');
+          $('#btnConta').addClass('btn-list');
+          $('#btnDados').removeClass('btn-list');
+          $('#btnDados').addClass('selecionado');
+          $('.dados').show('slow');
+        });
+       
     });
     </script>
   </body>
---
+++ perfil.php
@@ -0,0 +1,152 @@
+<?php 
+  include('layout/header.php');
+  include('layout/menu.php');
+
+ ?>
+<div class="container conteudo">
+	<div class="card">
+		<div class="card-header pintado text-center">
+			<h2>
+				Atualizando informações de perfil
+			</h2>
+		</div>
+		<div class="card-body">
+			<div class="row">
+				<div id="btnDados" class="col-6 btn btn-default selecionado">Suas informações</div>
+				<div id="btnConta" class="col-6 btn btn-default btn-list">Sua conta</div>
+			</div>
+			<div class="col-12">&nbsp;</div>
+
+			<form class="form-group dados">
+				<div class="form-row">
+					<div class="col-sm-12 col-md-8">
+						<div class="col-12">
+							<label for="nome">Nome : </label>
+							<input type="text" placeholder="João Machado" name="nome" class="form-control">
+						</div>
+						<div class="col-12">&nbsp;</div>
+						<div class="col-12">
+							<label>Data de nascimento : </label>
+							<input type="date" class="form-control" name="dtNasc">
+							<div class="col-sm-12">&nbsp;</div>
+
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-4">
+
+						<div class="dflex justify-content-center text-center">
+							<img src="img/profile-user.svg" class="img-fluid avatarh">
+							<div class="btn btn-success btn-gradient-success btn-rounded">
+								<input class="custom-file-input form-control-file" type="file" id="avatar"><span>Mudar foto</span> </input>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="col-sm-12 col-md-6">
+						<label>CEP</label>
+						<input type="text" id="cep" name="cep" class="form-control">
+					</div>
+					<div class="col-sm-12 col-md-6">
+						<label>Estado : </label>
+						<select class="js-example-basic-single form-control" id="uf" name="uf">
+							<option value="AC">Acre</option>
+						    <option value="AL">Alagoas</option>
+						    <option value="AP">Amapá</option>
+						    <option value="AM">Amazonas</option>
+						    <option value="BA">Bahia</option>
+						    <option value="CE">Ceará</option>
+						    <option value="DF">Distrito Federal</option>
+						    <option value="ES">Espírito Santo</option>
+						    <option value="GO">Goiás</option>
+						    <option value="MA">Maranhão</option>
+						    <option value="MT">Mato Grosso</option>
+						    <option value="MS">Mato Grosso do Sul</option>
+						    <option value="MG">Minas Gerais</option>
+						    <option value="PA">Pará</option>
+						    <option value="PB">Paraíba</option>
+						    <option value="PR">Paraná</option>
+						    <option value="PE">Pernambuco</option>
+						    <option value="PI">Piauí</option>
+						    <option value="RJ">Rio de Janeiro</option>
+						    <option value="RN">Rio Grande do Norte</option>
+						    <option value="RS">Rio Grande do Sul</option>
+						    <option value="RO">Rondônia</option>
+						    <option value="RR">Roraima</option>
+						    <option value="SC">Santa Catarina</option>
+						    <option value="SP">São Paulo</option>
+						    <option value="SE">Sergipe</option>
+						    <option value="TO">Tocantins</option>
+						</select>
+						<div class="col-sm-12">&nbsp;</div>
+
+					</div>
+					
+				</div>
+				<div class="form-row">
+					<div class="col-sm-12">
+						<label>Endereço : </label>
+						<input type="text" id="logradouro" class="form-control" name="end">
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="col-sm-12 col-md-6">
+						<label>Cidade : </label>
+						<input type="text" id="localidade" name="cidade" class="form-control">
+						<div class="col-sm-12">&nbsp;</div>
+					</div>
+					<div class="col-sm-12 col-md-6">
+						<label>bairro</label>
+						<input type="text" id="bairro" name="bairro" class="form-control">
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="col-12 text-center">
+						<button class="btn btn-success btn-gradient-success">Salvar mudanças</button>
+						
+					</div>
+				</div>
+					
+			</form>
+			<form class="form-group login" style="display: none;">
+				<div class="form-row">
+					<div class="col-sm-12 text-center">
+						<label>Email</label>
+						<input type="email" name="email" value="seuemail@email.com" disabled="" class="form-control">
+					</div>
+					<div class="col-12">&nbsp;</div>
+					<div class="col-sm-12 text-center">
+						<button type="button" class="btn btn-success btn-gradient-success" data-toggle="modal" data-target="#modalEmail">
+						  Mudar e-mail
+						</button>
+
+					</div>
+				</div>
+			</form>
+			<div class="modal fade" id="modalEmail" tabindex="-1" role="dialog" aria-labelledby="ModalEmail" aria-hidden="true">
+				<div class="modal-dialog modal-dialog-scrollable" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="ModalEmail"></h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+						...
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-primary">Save changes</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php 
+  include('layout/footer.php');
+ ?>
